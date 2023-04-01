17b4341044c599fa949813e5215591932404e8e4
vassilidev <vassilidevnet@gmail.com>
2023-04-01 10:15:32 +0200

    Reset Project

--- resources/views/project/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-site-layout>
-    <div class="flex flex-row flex-wrap ">
-        @foreach ($projects as $project)
-        <x-project-card :project="$project" />
-        @endforeach
-
-    </div>
-
-</x-site-layout>
