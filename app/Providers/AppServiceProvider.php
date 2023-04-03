3729d8c66271bd3440bcd8b6a4d7c87ff8a7bbe9
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-04-03 11:17:49 +0330

    fix: https issue on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment(['staging', 'production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
