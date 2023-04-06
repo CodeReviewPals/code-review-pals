06696221138bc0ea775bb83aa60e9c2367d16598
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-04-06 04:25:02 +0330

    #9 feat: soft delete and some more on repository

--- app/Models/Repository.php
+++ app/Models/Repository.php
@@ -4,8 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Repository extends Model
 {
+    use SoftDeletes;
     use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "about",
+        "tags",
+        "code_languages",
+    ];
+
+    /**
+     * cast tags to Array
+     *
+     * @return Attribute<string, never>
+     */
+    public function tags(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => explode(",", $value),
+            set: fn ($value) => implode(",", $value)
+        );
+    }
+
+    /**
+     * cast tags to Array
+     *
+     * @return Attribute<string, never>
+     */
+    public function code_languages(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => explode(",", $value),
+            set: fn ($value) => implode(",", $value)
+        );
+    }
 }
