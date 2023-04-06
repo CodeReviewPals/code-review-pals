0984f00ceb1e718f7895ff736fc7e756eea50c02
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-04-06 04:25:24 +0330

    #9 feat: ui add custom add button feature

--- resources/js/Components/Dashboard/Table/index.jsx
+++ resources/js/Components/Dashboard/Table/index.jsx
@@ -1,6 +1,14 @@
+import { Link } from "@inertiajs/react";
 import DashboardTablePagination from "../Pagination";
 
-export default function DashboardTable({ tableData, routePrefix, rowRender, suffixName }) {
+export default function DashboardTable({
+    tableData,
+    routePrefix,
+    rowRender,
+    suffixName,
+    columnsTitle,
+    customAddButton,
+}) {
     return (
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <section>
@@ -31,27 +39,30 @@ export default function DashboardTable({ tableData, routePrefix, rowRender, suff
                             />
                         </div>
                     </div>
+                    <div className="flex flex-row space-x-2">
+                        {customAddButton() || (
+                            <Link href={route(`${routePrefix}.create`)} className="flex items-center mt-4 gap-x-3">
+                                <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="w-5 h-5"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        />
+                                    </svg>
 
-                    <a href={route(`${routePrefix}.create`)} className="flex items-center mt-4 gap-x-3">
-                        <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="w-5 h-5"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                                />
-                            </svg>
-
-                            <span>Add {suffixName}</span>
-                        </button>
-                    </a>
+                                    <span>Add {suffixName}</span>
+                                </button>
+                            </Link>
+                        )}
+                    </div>
                 </div>
                 <div className="flex flex-col mt-6">
                     <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -60,72 +71,17 @@ export default function DashboardTable({ tableData, routePrefix, rowRender, suff
                                 <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                                     <thead className="bg-gray-50 dark:bg-gray-800">
                                         <tr>
-                                            <th
-                                                scope="col"
-                                                className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                            >
-                                                <button className="flex items-center gap-x-3 focus:outline-none">
-                                                    <span>Company</span>
-
-                                                    <svg
-                                                        className="h-3"
-                                                        viewBox="0 0 10 11"
-                                                        fill="none"
-                                                        xmlns="http://www.w3.org/2000/svg"
+                                            {columnsTitle.map((title, index) => {
+                                                return (
+                                                    <th
+                                                        key={index}
+                                                        scope="col"
+                                                        className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                                                     >
-                                                        <path
-                                                            d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z"
-                                                            fill="currentColor"
-                                                            stroke="currentColor"
-                                                            strokeWidth="0.1"
-                                                        />
-                                                        <path
-                                                            d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z"
-                                                            fill="currentColor"
-                                                            stroke="currentColor"
-                                                            strokeWidth="0.1"
-                                                        />
-                                                        <path
-                                                            d="M8.45558 7.25664V7.40664H8.60558H9.66065C9.72481 7.40664 9.74667 7.42274 9.75141 7.42691C9.75148 7.42808 9.75146 7.42993 9.75116 7.43262C9.75001 7.44265 9.74458 7.46304 9.72525 7.49314C9.72522 7.4932 9.72518 7.49326 9.72514 7.49332L7.86959 10.3529L7.86924 10.3534C7.83227 10.4109 7.79863 10.418 7.78568 10.418C7.77272 10.418 7.73908 10.4109 7.70211 10.3534L7.70177 10.3529L5.84621 7.49332C5.84617 7.49325 5.84612 7.49318 5.84608 7.49311C5.82677 7.46302 5.82135 7.44264 5.8202 7.43262C5.81989 7.42993 5.81987 7.42808 5.81994 7.42691C5.82469 7.42274 5.84655 7.40664 5.91071 7.40664H6.96578H7.11578V7.25664V0.633865C7.11578 0.42434 7.29014 0.249976 7.49967 0.249976H8.07169C8.28121 0.249976 8.45558 0.42434 8.45558 0.633865V7.25664Z"
-                                                            fill="currentColor"
-                                                            stroke="currentColor"
-                                                            strokeWidth="0.3"
-                                                        />
-                                                    </svg>
-                                                </button>
-                                            </th>
-
-                                            <th
-                                                scope="col"
-                                                className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                            >
-                                                Status
-                                            </th>
-
-                                            <th
-                                                scope="col"
-                                                className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                            >
-                                                About
-                                            </th>
-
-                                            <th
-                                                scope="col"
-                                                className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                            >
-                                                Users
-                                            </th>
-
-                                            <th
-                                                scope="col"
-                                                className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                                            >
-                                                License use
-                                            </th>
-
-                                            <th scope="col" className="relative py-3.5 px-4">
-                                                <span className="sr-only">Edit</span>
-                                            </th>
+                                                        {title}
+                                                    </th>
+                                                );
+                                            })}
                                         </tr>
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
--- resources/js/Pages/Dashboard/Repository/index.jsx
+++ resources/js/Pages/Dashboard/Repository/index.jsx
@@ -1,6 +1,6 @@
 import DashboardTable from "@/Components/Dashboard/Table";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 
 export default function Index({ auth, repositories }) {
     console.log(repositories);
@@ -80,6 +80,30 @@ export default function Index({ auth, repositories }) {
         );
     };
 
+    const addRepositoryFromThirdPartyButton = () => {
+        return (
+            <Link href={route(`dashboard.index`)} className="flex items-center mt-4 gap-x-3">
+                <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                    </svg>
+                    <span>Add</span>
+                </button>
+            </Link>
+        );
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -99,6 +123,8 @@ export default function Index({ auth, repositories }) {
                     routePrefix="dashboard.repository"
                     rowRender={rowRender}
                     suffixName="Repository"
+                    columnsTitle={["id", "Title", "About", "Tags", "Languages", "actions"]}
+                    customAddButton={addRepositoryFromThirdPartyButton}
                 />
             </div>
         </AuthenticatedLayout>
