14e3e17fae3e70089f8dab40afe8310826c1db13
vassilidev <vassilidevnet@gmail.com>
2023-04-09 09:25:40 +0200

    Refactor the existing PR

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
