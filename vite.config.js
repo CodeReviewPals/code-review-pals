d0b79f0f1e0baca1a0b674ed6526e85f4bf46937
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-04-11 10:00:08 +0330

    fix: VITE dev ssl disable on docker

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
 
+const serverSSL = (process.env.VITE_HTTPS || true) == true;
 export default defineConfig({
-    server: {https: true},
+    server: { https: serverSSL },
     plugins: [
         mkcert(),
         laravel({
