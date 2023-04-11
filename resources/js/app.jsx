da093c3beac3afb9bbb672a25204b86265fe06ec
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-04-11 17:50:20 +0330

    #4 refactor: title format

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -8,7 +8,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${appName} - ${title}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
