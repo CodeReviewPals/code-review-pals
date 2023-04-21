66b620c296cda7b62d156b2d1ed0f7b4f9d5462d
Vassili Joffroy <51865639+vassilidev@users.noreply.github.com>
2023-04-21 11:26:50 +0200

    fix: remove registered policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,31 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
-
-use App\Models\PullRequest;
-use App\Models\Repository;
-use App\Policies\PullRequestPolicy;
-use App\Policies\RepositoryPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        PullRequest::class => PullRequestPolicy::class,
-        Repository::class => RepositoryPolicy::class
-    ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-        $this->registerPolicies();
-    }
 }
