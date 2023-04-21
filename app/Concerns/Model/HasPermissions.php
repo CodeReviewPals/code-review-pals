3cbbeae60f34217b9c355939016b1f574d25b21f
Vassili Joffroy <51865639+vassilidev@users.noreply.github.com>
2023-04-21 11:49:58 +0200

    fix: move to protected to allow more check

--- app/Concerns/Model/HasPermissions.php
+++ app/Concerns/Model/HasPermissions.php
@@ -7,15 +7,15 @@ use App\Models\User;
 
 trait HasPermissions
 {
+    protected array $actions = ['view', 'update', 'delete', 'restore', 'forceDelete'];
+
     public function permissions(?User $user = null): array
     {
         $user = $user ?? Auth::user();
 
-        $actions = ['view', 'update', 'delete', 'restore', 'forceDelete'];
-
         $can = [];
 
-        foreach ($actions as $action) {
+        foreach ($this->actions as $action) {
             $can[$action] = $user?->can($action, $this) ?? false;
         }
 
