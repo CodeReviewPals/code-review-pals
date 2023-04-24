0b5f45bc057b1b255f4c8af7fafeccb020327a63
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-04-24 14:17:28 +0330

    #70 feat: repository_id on pull_requests table. because from now. all PR should have Repo

---
+++ database/migrations/2023_04_24_074700_add_repository_id_to_pull_requests_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Repository;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pull_requests', static function (Blueprint $table) {
+            $table->foreignIdFor(Repository::class, 'repository_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pull_requests', static function (Blueprint $table) {
+            $table->dropColumn('repository_id');
+        });
+    }
+};
