dae819dba11e3ec8c90e2ba82ca244302683d6c0
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-04-27 09:10:55 +0330

    #59 feat: schedule disable unavaliable webhooks commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('github:fetch-pull-request')->hourly();
+        $schedule->command('github:disable-unavailable-webhooks')->daily();
     }
 
     /**
