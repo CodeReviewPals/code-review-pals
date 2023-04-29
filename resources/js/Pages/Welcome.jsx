ebdd290d1780f8bc8d431c67394bb69cfaaa5ef9
HugoLaurent <lauhugopro@gmail.com>
2023-04-29 12:04:06 +0200

    feat: change button color dashboard

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -9,7 +9,7 @@ export default function Welcome({ auth, title }) {
             return (
                 <Link
                     href={route('dashboard')}
-                    className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+                    className="text-white inline-flex items-center bg-black border-0 py-1 px-3 focus:outline-none hover:bg-black rounded text-base mt-4 md:mt-0"
                 >
                     Dashboard
                     <svg
