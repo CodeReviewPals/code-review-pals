cbca974b59916f822499db50f34b7c28610e5d09
HugoLaurent <lauhugopro@gmail.com>
2023-04-29 21:37:33 +0200

    feat: main article set up

--- resources/js/Components/Button/Button.jsx
+++ resources/js/Components/Button/Button.jsx
@@ -1,11 +1,21 @@
-function Button({ text, size, color, logo, alt }) {
-    const className = `${size} ${color} inline-block mr-2`;
-    return (
-        <button className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-            <img className={className} src={logo} alt={alt} />
-            {text}
-        </button>
-    );
+function Button({ text, size, color, logo, alt, divers }) {
+    const className = `${size} ${color} ${divers} inline-block mr-2`;
+
+    if (!divers) {
+        return (
+            <button className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                <img className={className} src={logo} alt={alt} />
+                {text}
+            </button>
+        );
+    } else {
+        return (
+            <button className="bg-gray-200 hover:bg-gray-200 text-black  font-bold py-2 px-4 rounded border">
+                <img className={className} src={logo} alt={alt} />
+                {text}
+            </button>
+        );
+    }
 }
 
 export default Button;
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -2,6 +2,7 @@ import { Link, Head } from '@inertiajs/react';
 import logo from '../../images/Mainlogo.png';
 import GithubLogo from '../../images/dashboard/github.svg';
 import Button from '../Components/Button/Button';
+import pr from '../../images/pr.png';
 
 import { Title } from '@/Components/Title/Title';
 import './style.css';
@@ -32,7 +33,7 @@ export default function Welcome({ auth, title }) {
                 </div>
                 <nav>{returnAuthHeaderBtn()}</nav>
             </header>
-            <div className="bg-gray-300 drop-shadow-xl mx-auto mt-5 max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
+            <div className="main-div bg-gray-300 drop-shadow-xl mx-auto mt-5 max-w-8xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32 ">
                 <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
                     "Junior developer? Get a senior mentor to guide you to success."
                 </h1>
@@ -40,20 +41,20 @@ export default function Welcome({ auth, title }) {
                     "Our platform links you with senior developers and offers a supportive community."
                 </p>
                 <div className="mt-10 flex justify-center gap-x-6">
-                    <a
-                        className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-lg font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
-                        href="/register"
-                    >
-                        Look around
+                    <a className="group inline-flex items-center justify-center" href="/register">
+                        <Button
+                            size="w-8"
+                            text="Join us on Discord"
+                            color="invert-0"
+                            logo={GithubLogo}
+                            alt="Logo Github"
+                        />
                     </a>
                     <a
-                        className="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-lg focus:outline-none ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300"
+                        className="inline-flex items-center justify-center rounded-full py-2 px-4 text-lg"
                         href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
                     >
-                        <svg aria-hidden="true" className="h-3 w-3 flex-none fill-blue-600 group-active:fill-current">
-                            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z"></path>
-                        </svg>
-                        <span className="ml-3">Learn how to start</span>
+                        <Button size="w-6" text="Make your first Pull Request" divers={true} logo={pr} />
                     </a>
                 </div>
             </div>
