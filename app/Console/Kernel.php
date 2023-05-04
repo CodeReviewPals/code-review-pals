f152cfe440bded4d59c3a5a930bffdf3f6bd4c89
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-05-04 11:05:41 +0330

    feat: cleanup laravel faild queus

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('github:fetch-pull-request')->hourly();
+        $schedule->command('queue:flush --hours=24')->daily();
     }
 
     /**
