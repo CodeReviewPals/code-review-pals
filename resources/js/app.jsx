0dd510f2e2dafb0a4428efeeea74a3600d3d97d8
geeksesi <geeksesi@users.noreply.github.com>
2023-05-08 13:31:37 +0000

    style: prettier

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,7 +9,8 @@ const appName = window.document.getElementsByTagName('title')[0]?.innerText || '
 
 createInertiaApp({
     title: (title) => `${appName} - ${title}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) =>
+        resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
